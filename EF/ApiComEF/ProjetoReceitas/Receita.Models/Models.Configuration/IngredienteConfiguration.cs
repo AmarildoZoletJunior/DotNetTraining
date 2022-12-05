fc655999ec5195680bbd6649eb325ef9ee412980
Amarildo <amarildozj@gmail.com>
2022-12-05 14:06:25 -0300

    Arrumando Bug - PrimeiraApi

---
+++ EF/ApiComEF/ProjetoReceitas/Receita.Models/Models.Configuration/IngredienteConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receitas.Models.Models.Configuration
+{
+    public class IngredienteConfiguration : IEntityTypeConfiguration<Ingrediente>
+    {
+        void IEntityTypeConfiguration<Ingrediente>.Configure(EntityTypeBuilder<Ingrediente> builder)
+        {
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(X => X.IngredienteUnico).IsRequired();
+            builder.HasKey(x => x.Id);
+            
+        }
+    }
+}
